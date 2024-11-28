f1899353dc42603ac3adfe3b344cd4fb63a45e17
6704062617253 <s6704062617253@email.kmutnb.ac.th>
2024-11-28 18:02:02 +0700

    lab1 update1

--- lab1minmax.c
+++ lab1minmax.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 int main() {
 	int num1=100;
-	int num2=15;
+	int num2=150;
 	if (num1>num2){
 		printf("num1 is greater than num2");
 	}
-	if (num2>num1){
-		printf("num1 is greater than num2");
+	else{
+		printf("num2 is greater than num1");
 	}
 	
 }
